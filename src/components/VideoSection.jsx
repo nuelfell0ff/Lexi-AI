3cb1eb4350277fcbf580026dc6420363ee0a9a83
emmanuel123 <obaloluwaajayi2006@gmail.com>
2026-04-22 11:06:40 -0700

    changes the video to an embedded video link

--- src/components/VideoSection.jsx
+++ src/components/VideoSection.jsx
@@ -1,20 +1,20 @@
 import React from 'react'
 import './VideoSection.css'
-import videoFile from '../assets/lv_0_20260420235727.mp4'
 
 const VideoSection = () => {
   return (
     <div className="video-section">
       <div className="video-container">
-        <video
+        <iframe
           className="video-player"
-          controls
           width="100%"
           height="auto"
-        >
-          <source src={videoFile} type="video/mp4" />
-          Your browser does not support the video tag.
-        </video>
+          src="https://www.youtube.com/embed/OcLQkeyNK4c"
+          title="Lexi AI Video"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        ></iframe>
       </div>
     </div>
   )
